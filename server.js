a2fe141e771fb9b02ca620dbb44555adb75c493d
Petermuturi <pmuturi65@gmail.com>
2016-09-19 12:23:20 +0300

    added public folder

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
-	res.send("Hello there");
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 var port = Number(process.env.PORT || 3000);
