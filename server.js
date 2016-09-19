ff8cb4da1896b3c542f0349d5a6df4336bb88171
Petermuturi <pmuturi65@gmail.com>
2016-09-19 11:45:45 +0300

    removed typo on server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/' function(req,res){
-	res.send("Hello there")
+app.get('/', function(req,res){
+	res.send("Hello there");
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log('Server listening on port 3000');
+var port = Number(process.env.PORT || 3000);
+
+app.listen(port, function(){
+	console.log('server listening on port 3000');
 });
